71215d574f916753305635ac65b9e2d021313996
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-23 14:08:58 +0300

    basic logic

--- js/contact_me.js
+++ js/contact_me.js
@@ -58,15 +58,8 @@ $(function() {
             return $(this).is(":visible");
         },
     });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
 });
-
-
-/*When clicking on Full hide fail/success boxes */
+/*on focus on an input*/
 $('#name').focus(function() {
     $('#success').html('');
 });

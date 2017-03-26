dbe37aa5ab016feed2af124641496105454509cb
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-26 13:33:49 +0300

    finsih skills ajax

--- js/skills.js
+++ js/skills.js
@@ -1,30 +1,17 @@
-$('body').load(function(){
+$(document).ready(function(){
 	loadSkills();
-});
+})
 function loadSkills(){
-	$.ajax({
-		url:'../skills.json',
-		type:'GET'
-		dataType:json,
-		success:function(data){
-			showSkills(data)
-		},
-		fail:showError
-	})
-}
-function showSkills(json){
-	var data = JSON.stringify(json);
-	var html =''
-	html+= '<ul>'
-	for(i=0;i<data.length;i++){
+	$.get('skills.json',function(data){
+		var html =''
+		html+= '<ul>'
+			$.each(data.skills,function(i,res){
 		html += '<li>'
-		html+ ='<h2>'+ data[i].name + '</h2>'
-		html+= '<p>' + data[i].desc + '</p>'
+		html+='<h2>'+ res.name + '</h2>'
+		html+= '<p>' + res.desc + '</p>'
 		html+= '</li>'
-	}
-	html+ = '</ul>'
-	$('.container .success').html(html)
-}
-function showError(error){
-	$('.container .error').html(`${error}`);
+		html+= '</ul>'
+	})
+			$('.container #success').append(html)
+	});
 }

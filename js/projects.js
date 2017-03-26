5e9d3c21839334d75b0b9f6e9e2fc63599ec015f
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-26 14:30:00 +0300

    finsih current button ajax

--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,27 @@
-$('body').load(function(){
+$('.container #current').click(function(){
 	loadProjects();
-});
+})
 function loadProjects(token){
 	$.ajax({
-		url:'',
-		type:'GET'
-		dataType:jsonp,
-		beforeSend: function(xhr) {
-     		xhr.setRequestHeader("Authorization", "token " + token);
-    	},
+		url:'https://api.github.com/users/gooryalhamed/repos?sort=created',
+		type:'GET',
+		dataType:'json',
 		success:function(data){
 			showProjects(data)
 		},
 		fail:showError
 	})
 }
-function showProjects(){
-
+function showProjects(json){
+	var data = json.slice(0,5);
+	var html = '';
+	html+= '<div>';
+	$.each(data,function(i,item){
+		html+= '<h3>' + item.name+'</h3>'
+		html+= item.html_url;
+	});
+	$('.container .panel .panel-body').append(html);
 }
-function showError(){
+function showError(error){
 	$('.container .error').html(`${error}`);
 }

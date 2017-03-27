781b11376a5fb0187c01dfd490716ca8964b9683
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-27 15:08:47 +0300

    add date to projects

--- js/projects.js
+++ js/projects.js
@@ -28,6 +28,7 @@ function showProjects(json){
 	html+= '<div>';
 	$.each(data,function(i,item){
 		html+= '<a href="'+item.html_url+'" target="_blank">'+item.name+'</a>';
+		html+= '<span> created at' +item.created_at + '</span>'
 		html+= '<br>'
 	});
 	$('.container .panel .panel-body').append(html);

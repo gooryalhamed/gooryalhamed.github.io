8ba7a20457b5b9b8f4b566a162cf3983add9faae
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-04 15:49:51 +0300

    continue arrow event

--- js/about.js
+++ js/about.js
@@ -1,8 +1,17 @@
-$('.row #text i').click(function(){
-	showText()
+$('.row .mytext i').click(function(){
+	if($(this).attr('id')=="forward"){
+		showText1()
+	}else if($(this).attr('id')=="back"){
+		showText2()
+	}
 })
 
-function showText(){
-	var str = $('#secondp').text()
-	$('.row .mytext').html(str)
+function showText1(){
+	var str = $('#secondp').text();
+	$('.row .mytext').html(str);
+}
+
+function showText2(){
+	var str = $('.row #firstp').text();
+	$('.row .mytext').html(str);
 }

b77615783d5605691a145e3202718c485f7d6a96
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-06 23:20:21 +0300

    basic logic for using handlebars

--- js/more.js
+++ js/more.js
@@ -1,21 +1,18 @@
-$(document).ready(function(){
-	loadPlaylist();
-})
- function loadPlaylist(){
- 	$.ajax({
- 		url:'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyCdCswuvxVV4Qyif3OmX614mmy7MY07b9Y&playlistId=PL1CME6_KVTbRVnak-i_Zy8jcyr0jbXAD-',
- 		type:'GET',
- 		dataType:'json',
- 		success: function(data){
- 			showPlaylist(data)
- 		}
- 	});
- }
- function showPlaylist(json){
- 	var html= '';
- 	html+= '<ul>'
- 	$.each(json.items,function(i, item){
- 		html+= '<li><a href="https://www.youtube.com/watch?v='+item.snippet.resourceId.videoId+'&list='+item.snippet.playlistId+'"><img src="'+item.snippet.thumbnails.medium.url+'"</img></a><span>'+ item.snippet.title +'</span></li>';
- 	});
- 	$('.container #success').html(html);	
- }
+var url='https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyCdCswuvxVV4Qyif3OmX614mmy7MY07b9Y&playlistId=PL1CME6_KVTbRVnak-i_Zy8jcyr0jbXAD-';
+var request = new XMLHttpRequest();
+request.open('GET', url);
+request.onload = function(){
+	if (request.status >= 200 && request.status < 400){
+		var ourData = JSON.parse(request.responseText);
+		createHTML(ourData);
+	}else{
+		console.log("unable to retrieve data");
+	}
+}
+request.send();
+function createHTML(data){	
+	var rowTemplate = document.getElementById("videos-template").innerHTML;
+	var compiledTemplate = Handlebars.compile(rowTemplate);
+	var ourGeneratedHTML = compiledTemplate(data);
+	document.getElementById("videos-container").innerHTML = ourGeneratedHTML;
+}

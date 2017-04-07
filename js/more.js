9e40672d5fb68036b5cf2745fb79694cded8f286
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-07 16:16:49 +0300

    using handlebars to show videos

--- js/more.js
+++ js/more.js
@@ -1,18 +1,9 @@
 var url='https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyCdCswuvxVV4Qyif3OmX614mmy7MY07b9Y&playlistId=PL1CME6_KVTbRVnak-i_Zy8jcyr0jbXAD-';
-var request = new XMLHttpRequest();
-request.open('GET', url);
-request.onload = function(){
-	if (request.status >= 200 && request.status < 400){
-		var ourData = JSON.parse(request.responseText);
-		createHTML(ourData);
-	}else{
-		console.log("unable to retrieve data");
+$.ajax({
+	url:url,
+	type:'GET',
+	dataType:'json',
+	success: function(data){
+		const template = Handlebars.compile($('#videos-template').html())
+		$('#videos-container').html(template(data))
 	}
-}
-request.send();
-function createHTML(data){	
-	var rowTemplate = document.getElementById("videos-template").innerHTML;
-	var compiledTemplate = Handlebars.compile(rowTemplate);
-	var ourGeneratedHTML = compiledTemplate(data);
-	document.getElementById("videos-container").innerHTML = ourGeneratedHTML;
-}

c8e18d21c7a60ccfc2e79a1c902c9da4b74a6325
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-07 14:00:43 +0300

    using jquery with handlebars

--- js/skills.js
+++ js/skills.js
@@ -1,22 +1,7 @@
-var ourRequest =new XMLHttpRequest();
-ourRequest.open('GET', '../skills.json');
-ourRequest.onload = function(){
-	if (ourRequest.status >= 200 && ourRequest.status < 400){
-		var ourData = JSON.parse(ourRequest.responseText);
-		createHTML(ourData);
-	}else{
-		console.log("unable to retrieve data");
-	}
-};
-ourRequest.onerror = function(){
-	console.log("connection error");
-}
-ourRequest.send();
-
-function createHTML(skillsData){
-	var rowTemplate = document.getElementById("skills-template").innerHTML;
-	var compiledTemplate = Handlebars.compile(rowTemplate);
-	var ourGeneratedHTML = compiledTemplate(skillsData);
-	document.getElementById("skills-container").innerHTML = ourGeneratedHTML;
-}
-
+$(document).ready(function(){
+	var content = $('#skills-container');
+	var html = $('#skills-template');
+	var template = Handlebars.compile(html);
+	var data = $.get('../skills.json')
+	content.html() = template(data);
+})

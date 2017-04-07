0241bcfa3eb9ba343e87b7044792dd10bcf434cb
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-07 14:44:17 +0300

    handlebars with jquery

--- js/skills.js
+++ js/skills.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	var content = $('#skills-container');
-	var html = $('#skills-template');
+	var html = $('#skills-template').innerHTML;
 	var template = Handlebars.compile(html);
+		
 	var data = $.get('../skills.json')
 	content.html() = template(data);
 })

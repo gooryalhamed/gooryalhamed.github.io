d0904b5c144f1450fd7c2201a0eecb9e7d6862b4
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-07 16:17:33 +0300

    remove console.dir

--- js/skills.js
+++ js/skills.js
@@ -1,5 +1,4 @@
 $.get('../skills.json', function(data){
       const template = Handlebars.compile($('#skills-template').html())
-      console.dir(template)
       $('#skills-container').html(template(data))
 });

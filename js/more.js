24e43d4614c7f6a8aa16b4c1598de0ccf73eb479
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-11 23:07:09 +0300

    add spinner on loading

--- js/more.js
+++ js/more.js
@@ -1,3 +1,4 @@
+$('.container fa-spinner').fadeIn();
 var url='https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyCdCswuvxVV4Qyif3OmX614mmy7MY07b9Y&playlistId=PL1CME6_KVTbRVnak-i_Zy8jcyr0jbXAD-';
 $.ajax({
 	url:url,
@@ -7,4 +8,5 @@ $.ajax({
 		const template = Handlebars.compile($('#videos-template').html())
 		$('#videos-container').html(template(data))
 	}
-})
+});
+$('.container fa-spinner').fadeOut(1000);

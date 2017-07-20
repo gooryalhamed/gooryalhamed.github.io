5b1573dc9e00891bf689062dcf7222e209ac9be5
Goory al Hamed <gooryalhamed@hotmail.com>
2017-07-20 08:53:23 +0300

    refactor home page mwdia query

--- js/skills.js
+++ js/skills.js
@@ -1,4 +1,4 @@
 $.get('../skills.json', function(data){
-      const template = Handlebars.compile($('#skills-template').html())
-      $('#skills-container').html(template(data))
+	const template = Handlebars.compile($('#skills-template').html())
+	$('#skills-container').html(template(data))
 });
